2b4cd028d737d6645341318cb32af8d9fd4f8fe3
Carlos Quiros <carlosquiros12@gmail.com>
2021-02-04 18:00:51 -0500

    controller

--- src/controllers/productDes.controller.js
+++ src/controllers/productDes.controller.js
@@ -10,14 +10,18 @@ productCtrl.renderProductList = (req, res)=>{
 }
 
 productCtrl.newProduct = async (req, res) =>{
+    //obtengo el elemento enviado desde el post
     const {codigo, nombre, cantidad, precioU, img, categoria,colorDis, tallaDis} = req.body;
+    //creo el producto de acuerdo al modelo creado
     const newProduct = new product({codigo, nombre, cantidad, precioU, img, categoria, colorDis,tallaDis});
+    //realizo una busqueda para saber si existe alguno con el mismo codigo
     const pp = await product.findOne({codigo:newProduct.codigo});
+    //verifico si existe, 
     if(!pp){
         await newProduct.save();
         res.json('guardado')
     }else{
-        res.json(pp)
+        res.json("el elemento ya existe")
     }
 
     
